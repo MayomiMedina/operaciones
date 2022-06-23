8ed512d7befcd1f448a70852740c41b10b072e9d
Mayomi Medina <mmedina@canvia.com>
2022-06-23 15:18:35 -0500

    proyecto base, y interfaz de suma

---
+++ app/src/main/java/com/example/proyecto2/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.proyecto2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+BottomNavigationView btnNavigation;
+FrameLayout fragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+  asignarReferencias();
+    }
+    private void asignarReferencias(){
+        btnNavigation=findViewById(R.id.btnNavigation);
+        btnNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.menu_1){
+                    mostraFragmento(new fragmentoSuma());
+                    item.setChecked(true);
+                }
+                return false;
+            }
+        });
+
+    }
+    private void mostraFragmento(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+    }
+}
